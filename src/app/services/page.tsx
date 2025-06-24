82a9ffec1fcecd3e7023bcbe34e244d944a60ab9
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-06-24 18:45:40 +0530

    blog page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Blogs from '@/components/Services/Blogs'
+import RelatedArticles from "@/components/Services/RelatedArticles";
+
+function Page() {
+    return (
+        <div>
+            <Blogs/>
+            <RelatedArticles/>
+        </div>
+    );
+}
+
+export default Page;
